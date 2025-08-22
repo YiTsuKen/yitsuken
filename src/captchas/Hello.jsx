356f855dc9f9567d1cfa4afca9ad79b338311b9b
dexter3k <schooldev3000@gmail.com>
2025-08-22 17:48:58 +0300

    Tailwind stupidity

--- src/captchas/Hello.jsx
+++ src/captchas/Hello.jsx
@@ -1,7 +1,7 @@
 export default function Hello({ onComplete }) {
   return (
     <div>
-      <h2>Click the red dot!</h2>
+      <h2 className="">Click the red dot!</h2>
       <button onClick={onComplete}>🔴</button>
     </div>
   );
